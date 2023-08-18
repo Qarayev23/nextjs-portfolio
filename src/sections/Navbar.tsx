13aee99e57f63db75dedce97ca119f3d8bd630dd
nicat <qarayevn61@gmail.com>
2023-08-18 18:43:12 +0400

    update

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -123,7 +123,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link="http://localhost:3000/GarayevNijat.pdf" />
+            <Button text="Resume" link="/GarayevNijat.pdf" />
           </motion.div>
         </div>
       </div>
